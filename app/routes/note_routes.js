003ecd769e67fc9eebe28d2aec2fb03a90e32a73
Alper Silistre <alpersilistre@gmail.com>
2017-12-21 03:08:33 +0300

    Panic button server implemented.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,29 +1,90 @@
 let ObjectID = require('mongodb').ObjectID;
 const baseUrl = 'https://api.telegram.org/bot';
-const botApiToken = '483461591:AAGG0iNPgfQyEWJbf6glcuF1yBtyOjSD6ZA'
+const botApiToken = '483461591:AAGG0iNPgfQyEWJbf6glcuF1yBtyOjSD6ZA';
+const axios = require('axios');
+let path = require('path');
+let updateId;
+let newUpdateNumber;
+let updateIdObjectPrimaryKey;
 
 module.exports = function(app, db) {
-    app.post('/new-message', (req, res) => {
-        const {message} = req.body;
+    // app.post('/new-message', (req, res) => {
+    //     const {message} = req.body;
     
-        if(!message  || message.text.toLowerCase().indexOf('configure') < 0) {
-            return res.end();
-        }
-    
-        axios.post(`${baseUrl}${botApiToken}/sendMessage`, {
-            chat_id: message.chat.id,
-            text: 'Your account has been configured.'
-        })
-        .then(response => {
-            console.log('Message posted!');
-            res.end('OK');
-        })
-        .catch(err => {
-            console.log('Error:', err);
-            res.end('Error:', err);
-        });
-    
-        // chatId = message.chat.id;
+    //     if(!message  || message.text.toLowerCase().indexOf('configure') < 0) {
+    //         return res.end();
+    //     }
+    //     axios.post(`${baseUrl}${botApiToken}/sendMessage`, {
+    //         chat_id: message.chat.id,
+    //         text: 'Your account has been configured.'
+    //     })
+    //     .then(response => {
+    //         console.log('Message posted!');
+    //         res.end('OK');
+    //     })
+    //     .catch(err => {
+    //         console.log('Error:', err);
+    //         res.end('Error:', err);
+    //     });
+    // });
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname + '/../index.html'));
     });
+
+    app.get('/getUpdates', (req, res) => {        
+        db.collection('telegram').find({}).toArray((err, items) => {
+            if(err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {                
+                // console.log(items);
+                updateId = items[0].updateId;
+
+                let url = `${baseUrl}${botApiToken}/getUpdates?offset=${updateId}`;
+                // console.log(url);
+                axios.get(url)
+                .then(response => {
+                    if(response.data.result[0] !== undefined) {
+                        newUpdateNumber = parseInt(updateId) + 1;
+                        let details = { '_id': new ObjectID(items[0]._id) };
+                        let updatedObj = { updateId: newUpdateNumber };
+
+                        db.collection('telegram').update(details, updatedObj, (err, results) => {
+                            if(err) {
+                                res.send({ 'error': 'An error has occurred' });
+                            } else {
+                                //res.send(updatedObj);
+                                console.log('UpdateId updated...');
+                            }
+                        });
+                        // res.end(response.data.result[0].message.chat.id);
+                        res.end(response.data.result[0].message.chat.id.toString());
+                    } else {
+                        res.end('No new message');
+                    }
+                })
+                .catch(err => {
+                    console.log('Error:', err);
+                    res.end('Error:', err);
+                });
+            }
+        });        
+    });
+
+    
+
+    // app.get('/telegram', (req, res) => {
+    //     // const id = req.params.id;
+    //     // const details = { '_id': new ObjectID(id) };
+    //     db.collection('telegram').find({}).toArray((err, items) => {
+    //         if(err) {
+    //             res.send({ 'error': 'An error has occurred' });
+    //         } else {
+    //             // res.send(item);
+    //             console.log(items);
+    //             res.end('OK');
+    //         }
+    //     });
+    // });
     
 };
--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const axios = require('axios');
+// const db = require('./config/db');
 const port = 3000;
+let path = require('path');
 
 let chatId;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/app')));
 
-MongoClient.connect(db.url, (err, database) => {
+MongoClient.connect('mongodb://alpersilistre:123456@ds161336.mlab.com:61336/panicbutton', (err, database) => {
     const myDb = database.db('panicbutton');
     if (err) return console.log(err);
 
