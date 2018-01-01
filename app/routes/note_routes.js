7d4cfef2458a350ae6870782c43495d89091f90b
Alper Silistre <alpersilistre@gmail.com>
2018-01-01 15:12:53 +0300

    Remove unnecessary lines

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -8,25 +8,6 @@ let newUpdateNumber;
 let updateIdObjectPrimaryKey;
 
 module.exports = function(app, db) {
-    // app.post('/new-message', (req, res) => {
-    //     const {message} = req.body;
-    
-    //     if(!message  || message.text.toLowerCase().indexOf('configure') < 0) {
-    //         return res.end();
-    //     }
-    //     axios.post(`${baseUrl}${botApiToken}/sendMessage`, {
-    //         chat_id: message.chat.id,
-    //         text: 'Your account has been configured.'
-    //     })
-    //     .then(response => {
-    //         console.log('Message posted!');
-    //         res.end('OK');
-    //     })
-    //     .catch(err => {
-    //         console.log('Error:', err);
-    //         res.end('Error:', err);
-    //     });
-    // });
 
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname + '/../index.html'));
@@ -97,18 +78,4 @@ module.exports = function(app, db) {
         });
     });
 
-    // app.get('/telegram', (req, res) => {
-    //     // const id = req.params.id;
-    //     // const details = { '_id': new ObjectID(id) };
-    //     db.collection('telegram').find({}).toArray((err, items) => {
-    //         if(err) {
-    //             res.send({ 'error': 'An error has occurred' });
-    //         } else {
-    //             // res.send(item);
-    //             console.log(items);
-    //             res.end('OK');
-    //         }
-    //     });
-    // });
-    
 };
